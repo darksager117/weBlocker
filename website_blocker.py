703012be08de42dbf1aa4263df6c0d17b4ce9cc0
raulezama13 <lezamaral@yahoo.es>
2017-07-18 16:02:06 -0600

    Another repository

---
+++ website_blocker.py
@@ -0,0 +1,29 @@
+import time
+from datetime import datetime as dt
+
+
+host_temp= r"C:\Windows\System32\drivers\etc\hosts"   #Si hay una n antes de el backslash se tomara como special caracter, therefore es recomendable agregar 'r' al inicio
+redirect= "127.0.0.1"
+website_list=["www.facebook.com", "facebook.com", "www.youtube.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,14):
+        print("Working hours....")
+        with open(host_temp,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+ " "+ website +"\n")
+
+    else:
+        with open(host_temp, 'r+') as file:
+            content=file.readlines()  #Se toma cada linea como una lista para seleccionar lo que se va a cambiar
+            file.seek(0)   #se coloca el puntero al inicio
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+                file.truncate()
+            print("fun hours..")
+    time.sleep(1)
